027e914d6dadc8d45a10dfe24c72be337ccd5c8c
Cesar Val Leiva <cesarvalleiva@gmail.com>
2021-10-27 10:53:24 -0300

    change routes file

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,16 +1,11 @@
 
 import HomePage from '../pages/home.vue';
-import NotFoundPage from '../pages/404.vue';
 
 var routes = [
   {
     path: '/',
     component: HomePage,
-  },
-  {
-    path: '(.*)',
-    component: NotFoundPage,
-  },
+  }
 ];
 
 export default routes;
