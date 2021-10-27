a419b819788e420c0d589cfa3e9c0d70e40b7bcc
Cesar Val Leiva <cesarvalleiva@gmail.com>
2021-10-27 10:38:47 -0300

    first build

--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,16 @@ export default {
     outDir: BUILD_DIR,
     assetsInlineLimit: 0,
     emptyOutDir: true,
+    chunkSizeWarningLimit:1500,
+    rollupOptions: {
+      output:{
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+                return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
+        }
+      }
+    }
   },
   resolve: {
     alias: {
